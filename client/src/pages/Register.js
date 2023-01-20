468dea0a6498bc6776d9b9c0a6129ac6ec9d5159
Harshit <gharshit89@gmail.com>
2023-01-20 20:23:11 +0530

    update

---
+++ client/src/pages/Register.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Button, Checkbox, Form, Input } from 'antd';
+
+const Register = () => {
+  return (
+   <>
+    <div className="form-container">
+        <Form layout="vertical"></Form>
+    </div>
+   </>
+  )
+}
+
+export default Register
--- server/server.js
+++ server/server.js
@@ -1,8 +1,9 @@
 const express =require('express');
 const colors = require('colors');
 const morgan = require('morgan');
-
 require('dotenv').config()
+
+const PORT=process.env.PORT ||8080
 
 //rest objects
 const app=express();
@@ -14,5 +15,23 @@ app.use(morgan('dev'))
 
 //routes
 app.get('/',(req,res)=>{
-    res.status(200).send()
+    res.status(200).send({
+        message:"Server running.."
+    })
 })
+
+
+
+//port
+const start = async ()=>{
+    try {
+        // await connectDb(process.env.DATABASE);
+        app.listen(PORT,()=>{
+            console.log(`application running on ${PORT}`);
+        })
+    } catch (error) {
+        console.log("error")
+    }
+}
+
+start();
